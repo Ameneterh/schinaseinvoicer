5a29da5131aa90f76572841ddcb754302455386a
Amene <ameneterh@gmail.com>
2026-05-19 07:37:04 +0100

    editions to index file

--- server/index.js
+++ server/index.js
@@ -46,9 +46,9 @@ app.use("/server/business", businessRouter);
 // render deployment
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
 }
 
